92d60e9b409fa549e48838d978001bd5014e98a7
TomDillane <tommy.c.dillane@gmail.com>
2020-12-13 13:15:11 +0000

    emailjs piece fixed

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,7 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "rosie", {
         "project_request": contactForm.projectsummary.value,
-        "from_name": contactForm.name.value,
+        "from_name": contactForm.fullname.value,
         "from_email": contactForm.emailaddress.value
     })
     .then(
